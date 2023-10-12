1ae00c264ed40d675fefc081963c177b60428822
Helam7u7 <helam053@gmail.com>
2023-10-12 13:40:28 -0500

    d

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
-
+import './Content.css'
 const Content = () => {
   return (
-    <div className='container_pack'>Content</div>
+    <div className='container_pack'>
+      <div className='pack_digital'>
+        <h2>Área de Contenido Digital</h2>
+        <div className='pack_card'>
+          <div className='card'>
+            <div><img src="/img/packD/packB.png" alt="" /></div>
+            <h3>PAQUETE BÁSICO</h3>
+            <span className='btnMore'>
+              Ver más
+            </span>
+          </div>
+          <div className='card'>
+            <div><img src="/img/packD/packB.png" alt="" /></div>
+            <h3>PAQUETE BÁSICO</h3>
+            <span className='btnMore'>
+              Ver más
+            </span>
+          </div>
+          <div className='card'>
+            <div><img src="/img/packD/packU.png" alt="" /></div>
+            <h3>PAQUETE BÁSICO</h3>
+            <span className='btnMore'>
+              Ver más
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className='pack_digital'>
+        <h2>Área de Contenido Web</h2>
+        <div className='pack_card'>
+          <div className='card'>
+            <div><img src="/img/packW/Tableta1.png" alt="" /></div>
+            <h3>PAQUETE BÁSICO</h3>
+            <span className='btnMore'>
+              Ver más
+            </span>
+          </div>
+          <div className='card'>
+            <div><img src="/img/packW/Tableta2.png" alt="" /></div>
+            <h3>PAQUETE BÁSICO</h3>
+            <span className='btnMore'>
+              Ver más
+            </span>
+          </div>
+          <div className='card'>
+            <div><img src="/img/packW/Tableta3.png" alt="" /></div>
+            <h3>PAQUETE BÁSICO</h3>
+            <span className='btnMore'>
+              Ver más
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
