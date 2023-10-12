7102189ad725820fe26db6b64400dc762d498cbe
Helam7u7 <helam053@gmail.com>
2023-10-12 14:20:02 -0500

    pack

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,25 +3,25 @@ import './Content.css'
 const Content = () => {
   return (
     <div className='container_pack'>
-      <div className='pack_digital'>
+      <div className='pack pack_digital'>
         <h2>Área de Contenido Digital</h2>
         <div className='pack_card'>
           <div className='card'>
-            <div><img src="/img/packD/packB.png" alt="" /></div>
+            <div className='card_img'><img src="/img/packD/packB.png" alt="" /></div>
             <h3>PAQUETE BÁSICO</h3>
             <span className='btnMore'>
               Ver más
             </span>
           </div>
           <div className='card'>
-            <div><img src="/img/packD/packB.png" alt="" /></div>
+            <div className='card_img'><img src="/img/packD/packB.png" alt="" /></div>
             <h3>PAQUETE BÁSICO</h3>
             <span className='btnMore'>
               Ver más
             </span>
           </div>
           <div className='card'>
-            <div><img src="/img/packD/packU.png" alt="" /></div>
+            <div className='card_img uni'><img src="/img/packD/packU.png" alt="" /></div>
             <h3>PAQUETE BÁSICO</h3>
             <span className='btnMore'>
               Ver más
@@ -29,25 +29,25 @@ const Content = () => {
           </div>
         </div>
       </div>
-      <div className='pack_digital'>
+      <div className='pack pack_web'>
         <h2>Área de Contenido Web</h2>
         <div className='pack_card'>
           <div className='card'>
-            <div><img src="/img/packW/Tableta1.png" alt="" /></div>
+            <div className='card_img'><img src="/img/packW/Tableta1.png" alt="" /></div>
             <h3>PAQUETE BÁSICO</h3>
             <span className='btnMore'>
               Ver más
             </span>
           </div>
           <div className='card'>
-            <div><img src="/img/packW/Tableta2.png" alt="" /></div>
+            <div className='card_img'><img src="/img/packW/Tableta2.png" alt="" /></div>
             <h3>PAQUETE BÁSICO</h3>
             <span className='btnMore'>
               Ver más
             </span>
           </div>
           <div className='card'>
-            <div><img src="/img/packW/Tableta3.png" alt="" /></div>
+            <div className='card_img'><img src="/img/packW/Tableta3.png" alt="" /></div>
             <h3>PAQUETE BÁSICO</h3>
             <span className='btnMore'>
               Ver más
