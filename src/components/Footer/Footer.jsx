43ea4c3c16bb67a53d14701d01c1502f458ce9cc
Helam7u7 <helam053@gmail.com>
2023-10-13 11:25:51 -0500

    homeView

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,27 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <div className='footer_inf'>
+        <div className='inf inf_services'>
+          <h3 className='inf_title'>Servicios</h3>
+          <p>Área de Contenido Digital</p>
+          <p>Área de Contenido Web</p>
+        </div>
+        <div className='inf inf_contact'>
+          <h3 className='inf_title'>Contacto</h3>
+          <p>Cel/Whapp: +51 999 999 999</p>
+          <p>Email: agenciacreativeperu@gmail.com</p>
+        </div>
+      </div>
+      <div className='footer_map'>
+        <div><img src="/svg/mapa.png" alt="" /></div>
+      </div>
+      <div className='footer_wsp'>
+        <div><img src="/svg/qr.png" alt="" /></div>
+        <div><img src="/svg/btnwsp.png" alt="" /></div>
+      </div>
+    </div>
   )
 }
 
